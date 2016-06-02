7609f9435145c1fc6c828c1c5d45fea59412b1f6
lam92 <hiumathy@gmail.com>
2016-06-02 17:53:34 -0500

    top bar

--- app/src/main/java/com/example/flow/projectflow/MainActivity.java
+++ app/src/main/java/com/example/flow/projectflow/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.flow.projectflow;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.hide();
 
         email = (EditText) findViewById(R.id.userEmail);
     }
